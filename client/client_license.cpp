642c9c93734be816dcc3371925d526663349c8a5
rabo <raboserg@gmail.com>
2019-12-23 00:28:40 +0300

    small change rest client

--- client/client_license.cpp
+++ client/client_license.cpp
@@ -8,7 +8,7 @@
 
 // /rest/host/get-host-licenses
 // requestDTO
-/* 
+/*
 {
   "file": "string",
   "mac": "string",
@@ -17,26 +17,32 @@
 */
 
 int main_run() {
+  // web::http::client::http_client sdfdsf("udfsds");
+  // sdfdsf.request("sssss").
+
   // Create user data as JSON object and make POST request.
   auto postJson =
       pplx::create_task([]() {
         web::json::value request;
+
+        web::http::client::http_client_config config;
+        config.set_timeout(utility::seconds(30));
+
         request[U("file")] = web::json::value::string(U("file"));
         request[U("mac")] = web::json::value::string(U("mac"));
         request[U("unp")] = web::json::value::string(U("unp"));
-
-        return web::http::client::http_client(U("http://localhost:9090"))
-            .request(web::http::methods::POST,
-                     web::http::uri_builder(U("http://localhost:9090"))
-                         .append_path(U("/res"))
-                         .to_string(),
-                     request.serialize(), U("application/json"));
+        web::http::uri address = U("http://localhost:9090");
+        return web::http::client::http_client(address, config)
+            .request(
+                web::http::methods::POST,
+                web::http::uri_builder().append_path(U("/res")).to_string(),
+                request.serialize(), U("application/json"));
       })
           .then([](web::http::http_response response) { // Get the response.
             // Check the status code.
             ucout << response.to_string() << std::endl;
             ucout << response.status_code() << std::endl;
-            if (response.status_code() != 201) {
+            if (response.status_code() == 201) {
               throw std::runtime_error("Returned " +
                                        std::to_string(response.status_code()));
             }
