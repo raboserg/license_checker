a0e39e03b58c7336c28d5f4c12e5868a87fad64c
Rabo Serg <raboserg@gmail.com>
2020-02-12 16:08:53 +0300

    build system

--- build.bat
+++ build.bat
@@ -8,8 +8,9 @@ rem mkdir build
 cd build
 rem cmake .. -DCMAKE_PREFIX_PATH=D:/library/vcpkg/installed/x64-windows -DCMAKE_TOOLCHAIN_FILE=D:/library/vcpkg/scripts/buildsystems/vcpkg.cmake -DBoost_ADDITIONAL_VERSIONS=1.70.0;1.71.0 -DCMAKE_BUILD_TYPE=Debug
 rem cmake .. -A x64 -DCMAKE_TOOLCHAIN_FILE=D:/library/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=Debug
-cmake .. -A x64 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows ..
-rem cmake .. -A x64 -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x64-windows -DVCPKG_BUILD_TYPE=release..
+rem cmake .. -A x64 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+rem cmake .. -A x64 -DCMAKE_BUILD_TYPE=Debug -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake -DVCPKG_TARGET_TRIPLET=x86-windows-static ..
+cmake .. -A x64 -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 rem cmake .. -A x64 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MD" ..
 
 
