bcffbb7d8f504682d49bee10dd7bca31a6e2c139
rabo <raboserg@gmail.com>
2020-03-27 00:57:55 +0300

    clang-format, added change with handler

--- nx_service/config_change.cpp
+++ nx_service/config_change.cpp
@@ -7,30 +7,29 @@
 namespace itvpnagent {
 
 Config_Handler::Config_Handler(ACE_Reactor *r)
-    : ACE_Event_Handler (r), handle_(ACE_INVALID_HANDLE) {
-  this->reactor(r);
+    : ACE_Event_Handler(r), handle_(ACE_INVALID_HANDLE) {
+  // this->reactor(r);
 
   this->set_file_name(PARSER::instance()->get_config_file_name());
 
-  const string directory = System::get_path_without_file_name(
-      PARSER::instance()->get_service_path().c_str());
+  const string directory =
+      System::get_path_without_file_name(PARSER::instance()->get_service_path().c_str());
   this->set_directory(directory);
 
   char_t buffer[BUFSIZ];
-  const size_t len =
-    ACE_OS::sprintf(buffer, "Started monitoring file '%s' ...\n", PARSER::instance()->get_service_path().c_str());
-    ACE_DEBUG((LM_DEBUG, "%T Config_Handler: %s ", buffer, "(%t) \n"));
-    INFO_LOG(buffer);
-
+  const size_t len = ACE_OS::sprintf(buffer, "Started monitoring file '%s' ...\n",
+                                     PARSER::instance()->get_service_path().c_str());
+  ACE_DEBUG((LM_DEBUG, "%T Config_Handler: %s ", buffer, "(%t) \n"));
+  INFO_LOG(buffer);
 
   this->handle_ = inotify_init();
-  this->watch_ = inotify_add_watch(this->handle_, this->get_directory().c_str(), IN_MODIFY ); 
+  this->watch_ =
+      inotify_add_watch(this->handle_, this->get_directory().c_str(), IN_MODIFY);
 
   if (this->handle_ == ACE_INVALID_HANDLE)
     ACE_ERROR((LM_ERROR, "FindFirstChangeNotification could not be setup\n"));
 
-  if (this->reactor()->register_handler(this, ACE_Event_Handler::READ_MASK) ==
-      -1)
+  if (this->reactor()->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
     ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) Echo_Handler::open: %p\n"),
                ACE_TEXT("register_handler for input")));
 }
@@ -45,33 +44,34 @@ ACE_HANDLE Config_Handler::get_handle(void) const { return handle_; }
 int Config_Handler::handle_input(ACE_HANDLE) {
   ACE_DEBUG((LM_DEBUG, "%T Config_Handler::handle_input (%t) \n"));
   char buffer[EVENT_BUF_LEN];
+  // clang-format off
+  // clang-format on
   if (read(this->handle_, buffer, EVENT_BUF_LEN) < 0)
     ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("length < 0")), -1);
-  struct inotify_event *event = (struct inotify_event *)&buffer;
+  const struct inotify_event *event = (struct inotify_event *)&buffer;
   if (event->len) {
-      if (event->mask & IN_ISDIR) {
-          ACE_DEBUG((LM_DEBUG, ACE_TEXT("%T Directory %s modify (%t)\n"), event->name));
-      } else {
-          if (event->mask & IN_MODIFY){
-              ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) Config_Handler: File %s modify.\n"),
-                         event->name));
-              if (ACE_OS::strcmp(this->get_file_name().c_str(), event->name) == 0) {
-                  char_t buffer[BUFSIZ];
-                  const size_t len =
-                          ACE_OS::sprintf(buffer, "The %s was update\n", event->name);
-                  ACE_DEBUG((LM_DEBUG, "%T Config_Handler::processing ", buffer, "(%t) \n"));
-                  INFO_LOG(buffer);
-                  ACE_OS::sleep(1);
-                  if (PARSER::instance()->init() == -1) {
-                      ACE_ERROR((LM_ERROR, "%T %p: Cannot to initialize constants (%t) \n",
-                                 "\tConfig_Handler::handle_input"));
-                      raise(SIGINT);
-                  } else {
-                      SERVICE::instance()->reshedule_tasks();
-                  }
-              }
+    if (event->mask & IN_ISDIR) {
+      ACE_DEBUG((LM_DEBUG, ACE_TEXT("%T Directory %s modify (%t)\n"), event->name));
+    } else {
+      if (event->mask & IN_MODIFY) {
+        ACE_DEBUG(
+            (LM_DEBUG, ACE_TEXT("(%t) Config_Handler: File %s modify.\n"), event->name));
+        if (ACE_OS::strcmp(this->get_file_name().c_str(), event->name) == 0) {
+          char_t buffer[BUFSIZ];
+          const size_t len = ACE_OS::sprintf(buffer, "The %s was update\n", event->name);
+          ACE_DEBUG((LM_DEBUG, "%T Config_Handler::processing ", buffer, "(%t) \n"));
+          INFO_LOG(buffer);
+          ACE_OS::sleep(1);
+          if (PARSER::instance()->init() == -1) {
+            ACE_ERROR((LM_ERROR, "%T %p: Cannot to initialize constants (%t) \n",
+                       "\tConfig_Handler::handle_input"));
+            raise(SIGINT);
+          } else {
+            SERVICE::instance()->reshedule_tasks();
           }
+        }
       }
+    }
   }
   return 0;
 }
@@ -98,4 +98,4 @@ void Config_Handler::set_file_name(const std::string &file_name) {
   this->file_name_ = file_name;
 }
 
-}  // namespace itvpnagent
+} // namespace itvpnagent
--- nx_service/itvpn_exec_handler.cpp
+++ nx_service/itvpn_exec_handler.cpp
@@ -1,15 +1,15 @@
 #include "itvpn_exec_handler.h"
 #include "ace/OS_NS_string.h"
+#include "event_sink_task.h"
 #include "parser_ini.h"
 #include "tools.h"
 #include "tracer.h"
-#include "event_sink_task.h"
 
 namespace itvpnagent {
 
-Itvpn_Exec_Handler::Itvpn_Exec_Handler(ACE_Reactor *reactor)
-    : handle_(ACE_INVALID_HANDLE) {
-  this->reactor(reactor);
+Itvpn_Exec_Handler::Itvpn_Exec_Handler(ACE_Reactor *r)
+    : ACE_Event_Handler(r), handle_(ACE_INVALID_HANDLE) {
+  // this->reactor(reactor);
 
   const std::string file_name = System::get_file_name_from_path(
       PARSER::instance()->options().openvpn_file_path.c_str());
@@ -19,24 +19,21 @@ Itvpn_Exec_Handler::Itvpn_Exec_Handler(ACE_Reactor *reactor)
       PARSER::instance()->options().openvpn_file_path.c_str());
   this->set_directory(directory);
 
-
   char_t buffer[BUFSIZ];
   const size_t len =
-    ACE_OS::sprintf(buffer, "Started monitoring file '%s'...\n", 
-      PARSER::instance()->options().openvpn_file_path.c_str());
-    ACE_DEBUG((LM_DEBUG, "%T Itvpn_Exec_Handler: %s ", buffer, "(%t) \n"));
-    INFO_LOG(buffer);
-
+      ACE_OS::sprintf(buffer, "Started monitoring file '%s'...\n",
+                      PARSER::instance()->options().openvpn_file_path.c_str());
+  ACE_DEBUG((LM_DEBUG, "%T Itvpn_Exec_Handler: %s ", buffer, "(%t) \n"));
+  INFO_LOG(buffer);
 
   this->handle_ = inotify_init();
-  this->watch_ = inotify_add_watch(this->handle_, this->get_directory().c_str(), IN_OPEN ); 
-
   if (this->handle_ == ACE_INVALID_HANDLE)
-    ACE_ERROR((LM_ERROR, "FindFirstChangeNotification could not be setup\n"));
+    ACE_ERROR((LM_ERROR, "Inotify handler could not be setup\n"));
 
-  if (this->reactor()->register_handler(this, ACE_Event_Handler::READ_MASK) ==
-      -1)
-    ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) Echo_Handler::open: %p\n"),
+  this->watch_ = inotify_add_watch(this->handle_, this->get_directory().c_str(), IN_OPEN);
+
+  if (this->reactor()->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
+    ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) Itvpn_Exec_Handler::open: %p\n"),
                ACE_TEXT("register_handler for input")));
 }
 
@@ -52,29 +49,43 @@ int Itvpn_Exec_Handler::handle_input(ACE_HANDLE) {
   char buffer[EVENT_BUF_LEN];
   if (read(this->handle_, buffer, EVENT_BUF_LEN) < 0)
     ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("length < 0")), -1);
-  struct inotify_event *event = (struct inotify_event *)&buffer;
+  const struct inotify_event *event = (struct inotify_event *)&buffer;
   if (event->len) {
-      if (event->mask & IN_ISDIR) {
-          ACE_DEBUG((LM_DEBUG, ACE_TEXT("%T Directory %s modify (%t)\n"), event->name));
-      } else {
-          if (event->mask & IN_OPEN){
-              if (ACE_OS::strcmp(this->get_file_name().c_str(), event->name) == 0) {
-                  char_t buffer[BUFSIZ];
-                  const size_t len =
-                          ACE_OS::sprintf(buffer, "The %s is opening...\n", event->name);
-                  ACE_DEBUG((LM_DEBUG, "%T Itvpn_Exec_Handler::processing ", buffer, "(%t) \n"));
-                  INFO_LOG(buffer);
-                  ACE_OS::sleep(1);
-                  LICENSE_WORKER_TASK::instance()->open();
-              }
-          }
+    if (event->mask & IN_ISDIR) {
+      ACE_DEBUG((LM_DEBUG, ACE_TEXT("%T Directory %s modify (%t)\n"), event->name));
+    } else {
+      if (event->mask & IN_OPEN) {
+        if (ACE_OS::strcmp(this->get_file_name().c_str(), event->name) == 0) {
+          char_t buffer[BUFSIZ];
+          const size_t len =
+              ACE_OS::sprintf(buffer, "The %s is opening...\n", event->name);
+          ACE_DEBUG((LM_DEBUG, "%T Itvpn_Exec_Handler::processing ", buffer, "(%t) \n"));
+          INFO_LOG(buffer);
+          ACE_OS::sleep(1);
+          LICENSE_WORKER_TASK::instance()->open();
+
+          //
+          this->reactor()->suspend_handler(this);
+          // Remove for writing
+          ACE_Reactor_Mask mask =
+              ACE_Event_Handler::WRITE_MASK | ACE_Event_Handler::DONT_CALL;
+          this->reactor()->remove_handler(this, mask);
+          // event->signal();
+        }
       }
+    }
   }
   return 0;
 }
 
 int Itvpn_Exec_Handler::handle_signal(int, siginfo_t *, ucontext_t *) {
   ACE_DEBUG((LM_DEBUG, "(%t) Itvpn_Exec_Handler::handle_signal\n"));
+  //
+  this->reactor()->resume_handler((this);
+
+  if (this->reactor()->register_handler(this, ACE_Event_Handler::READ_MASK) == -1)
+    ACE_ERROR((LM_ERROR, ACE_TEXT("(%P|%t) Itvpn_Exec_Handler::open: %p\n"),
+               ACE_TEXT("register_handler for input")));
   return 0;
 }
 
@@ -95,4 +106,4 @@ void Itvpn_Exec_Handler::set_file_name(const std::string &file_name) {
   this->file_name_ = file_name;
 }
 
-}  // namespace itvpnagent
+} // namespace itvpnagent
