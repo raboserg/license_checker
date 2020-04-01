1fd8cd1db60a28a081683a6080032dbaf24cb399
Rabo Serg <raboserg@gmail.com>
2020-04-01 20:12:30 +0300

    small update build system

--- build.bat
+++ build.bat
@@ -6,6 +6,8 @@ rem mkdir build
 
 cd build
 rem cmake .. -A x64 -DCMAKE_BUILD_TYPE=Release -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+rem cmake .. -DCMAKE_CXX_FLAGS:STRING="-m32" -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x86-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+rem cmake .. -A x86 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x86-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 cmake .. -A x64 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x64-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 rem --config Release
 rem cmake .. -A x86 -DCMAKE_BUILD_TYPE=Debug -DVCPKG_TARGET_TRIPLET=x86-windows -DRUNTIME="MT" -DCMAKE_TOOLCHAIN_FILE=D:\library\vcpkg\scripts\buildsystems\vcpkg.cmake ..
